ed2ff768b8fc15226b8560d2bfaab31f9c181cb9
samart.chaiprasittikul <samart.chaiprasittikul@krungsri.com>
2024-04-14 21:12:11 +0700

    Update login

--- angular-homework/src/app/login/login.component.ts
+++ angular-homework/src/app/login/login.component.ts
@@ -1,44 +1,58 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button'; // Import MatButtonModule for mat-button
 import { MatCardModule } from '@angular/material/card'; // Import MatCardModule for mat-card
 import { MatIconModule }  from '@angular/material/icon';
-import {FormsModule} from '@angular/forms';
-
-import { LoginForm } from '../models/login/login-form';
+import { FormsModule } from '@angular/forms';
+import { LoginForm } from '@app/models/login-form';
+import { AuthService } from '@app/services/auth.service';
+import { ValidationErrorComponent } from "@app/validation-error/validation-error.component";
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  imports: [
-    MatSelectModule, 
-    MatInputModule, 
-    MatFormFieldModule,
-    MatButtonModule,
-    MatIconModule,
-    MatCardModule,
-    FormsModule
-  ],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+    selector: 'app-login',
+    standalone: true,
+    templateUrl: './login.component.html',
+    styleUrl: './login.component.css',
+    imports: [
+        MatSelectModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatButtonModule,
+        MatIconModule,
+        MatCardModule,
+        FormsModule,
+        CommonModule,
+        ValidationErrorComponent,
+    ]
 })
 export class LoginComponent {
 
   hide = true;
   form = new LoginForm();
 
-  constructor() {
+  constructor(private authService: AuthService,) {
 
   }
 
   onSubmit() {
 
+   
     const { username, password } = this.form;
-
+    console.log(username);
     console.log(password);
+
+    this.authService.login(username, password).subscribe({
+      next: data => {
+        console.log(data);
+      },
+      error: err => {
+        console.error(err);
+        console.log(err);
+      }
+    });
 
   }
 
