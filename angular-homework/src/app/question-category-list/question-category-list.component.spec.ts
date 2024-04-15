683bb4708f232ce0a3c9921d072b35a3ea6ff89f
samart.chaiprasittikul <samart.chaiprasittikul@krungsri.com>
2024-04-15 14:09:00 +0700

    Added component question list

---
+++ angular-homework/src/app/question-category-list/question-category-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { QuestionCategoryListComponent } from './question-category-list.component';
+
+describe('QuestionCategoryListComponent', () => {
+  let component: QuestionCategoryListComponent;
+  let fixture: ComponentFixture<QuestionCategoryListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [QuestionCategoryListComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(QuestionCategoryListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ angular-homework/src/app/question-category-list/question-category-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-question-category-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './question-category-list.component.html',
+  styleUrl: './question-category-list.component.css'
+})
+export class QuestionCategoryListComponent {
+
+}
