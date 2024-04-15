82feef7c185da6a6462ab47d42300fdef21ba289
samart.chaiprasittikul <samart.chaiprasittikul@krungsri.com>
2024-04-15 11:56:13 +0700

    Updated route for use  auth guard

--- angular-homework/src/app/app.routes.ts
+++ angular-homework/src/app/app.routes.ts
@@ -1,7 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { QuestionCategoryListComponent } from './question-category-list/question-category-list.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
 
     { path: 'login', component: LoginComponent },
+    { path: 'category', component: QuestionCategoryListComponent , canActivate: [authGuard] },
 ];
